345a7a74a043a85a799bd2e4efeca47e30c8ab3c
Sasha <sashajulkin@gmail.com>
2023-04-06 22:30:47 +0200

    Charts and info cards have been added

---
+++ Weight Tracker/UserControls/BarChart.xaml.cs
@@ -0,0 +1,50 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weight_Tracker.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BarChart.xaml
+    /// </summary>
+    public partial class BarChart : UserControl
+    {
+        public BarChart()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+                    {
+                        new ColumnSeries
+                        {
+                           Values = new ChartValues<double> { 79.2, 78.8, 79.0, 78.7, 76.6, 74.9, 74.1, 73.3, 73.5, 73.5, 74.0, 73.1, 74.4 }
+                        }
+                    };            
+
+            Labels = new[] { "Mar 22", "Apr 22", "May 22", "Jun 22", "Jul 22", "Aug 22", "Sep 22", "Oct 22", "Nov 22", "Dec 22", "Jan 23", "Feb 23", "Mar 23", };
+            Formatter = value => value.ToString("N1");
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
+    }
+}
+
+     
---
+++ Weight Tracker/UserControls/InfoCard.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weight_Tracker.InfoCard
+{    
+    public partial class InfoCard : UserControl
+    {
+        public InfoCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register("Title", typeof(string), typeof(InfoCard));
+
+        public string Number
+        {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumberProperty =
+            DependencyProperty.Register("Number", typeof(string), typeof(InfoCard));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(InfoCard));
+
+        public bool IsTechnical
+        {
+            get { return (bool)GetValue(IsTechnicalProperty); }
+            set { SetValue(IsTechnicalProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsTechnicalProperty =
+            DependencyProperty.Register("IsTechnical", typeof(bool), typeof(InfoCard));
+    }
+}
